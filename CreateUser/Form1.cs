b098f6ca799207e3adaea77f1114931797796924
Neophear <stiiggade@gmail.com>
2019-10-11 09:40:34 +0200

    Initial commit

---
+++ CreateUser/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateUser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RunspaceConfiguration runspaceConfiguration = RunspaceConfiguration.Create();
+
+            Runspace runspace = RunspaceFactory.CreateRunspace(runspaceConfiguration);
+            runspace.Open();
+
+            RunspaceInvoke scriptInvoker = new RunspaceInvoke(runspace);
+
+            Pipeline pipeline = runspace.CreatePipeline();
+
+            //Here's how you add a new script with arguments
+            Command myCommand = new Command(System.IO.Path.GetFullPath("script.ps1"));
+
+            pipeline.Commands.Add(myCommand);
+
+            // Execute PowerShell script
+            System.Collections.ObjectModel.Collection<PSObject> collection = pipeline.Invoke();
+
+            foreach (PSObject obj in collection)
+            {
+                textBox2.AppendText(obj.ToString());
+            }
+        }
+    }
+}
